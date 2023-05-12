81745d675f502595e17dc5cf971b048dadcb0c04
bluluke <bluluke@hotmail.com>
2023-05-12 21:33:10 +0100

    I have completed the -are conjugator and it seems to be working fine.

--- areConjugator.js
+++ areConjugator.js
@@ -1,6 +1,10 @@
 
 
-//This is a function to takes a verb, removes the suffix and produces all of the present conjugations for that verb
-const conjugateAre = (stem) => {
+//This is a function to takes an infinitive, removes the suffix and produces all of the present conjugations for that verb
+const conjugateAre = (infinitive) => {
+    const stem = infinitive.slice(0, infinitive.length -3);
+    return `Io ${stem}o \nTu ${stem}i \nLui/Lei ${stem}a \nNoi ${stem}iamo \nVoi ${stem}ate \nLoro ${stem}ano`;
 
 }
+
+console.log(conjugateAre('parlare'));
