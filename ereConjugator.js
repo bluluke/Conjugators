6302032ef3e00468aa77b5a5b30638d14e30f2e9
bluluke <bluluke@hotmail.com>
2023-05-12 21:45:11 +0100

    I have completed the -ere conjugator.

--- ereConjugator.js
+++ ereConjugator.js
@@ -1,1 +1,9 @@
 
+//This is a function to takes an infinitive ending in -ere, removes the suffix and produces all of the present conjugations for that verb
+const conjugateEre = (infinitive) => {
+    const stem = infinitive.slice(0, infinitive.length -3);
+    return `Io ${stem}o \nTu ${stem}i \nLui/Lei ${stem}e \nNoi ${stem}iamo \nVoi ${stem}ete \nLoro ${stem}ono`;
+
+}
+
+console.log(conjugateEre('vivere'));
