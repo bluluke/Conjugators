6006af56661f3f40051e9c5caa17262d6c3d201e
bluluke <bluluke@hotmail.com>
2023-05-12 21:39:14 +0100

    Created function to produce conjucations of -ire verbs.

--- ireConjugator.js
+++ ireConjugator.js
@@ -1,1 +1,8 @@
+//This is a function to takes an infinitive with an -ire suffix, removes the suffix and produces all of the present conjugations for that verb
+const conjugateIre = (infinitive) => {
+    const stem = infinitive.slice(0, infinitive.length -3);
+    return `Io ${stem}isco \nTu ${stem}isci \nLui/Lei ${stem}isce \nNoi ${stem}iamo \nVoi ${stem}ite \nLoro ${stem}iscono`;
 
+}
+
+console.log(conjugateIre('finire'));
